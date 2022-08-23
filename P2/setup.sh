7afe8c4083ab828ed688b25f280e1701ecdd12ab
Maxime KAYUMBA ONAKASONGO <mkayumba@e2r12p7.clusters.42paris.fr>
2022-08-23 14:49:51 +0200

    init P2

---
+++ P2/setup.sh
@@ -0,0 +1,25 @@
+
+set -e
+#create a interface br0 :
+ip link add br0 type bridge
+
+#bring up interface br0 :
+ip link set dev br0 up
+
+#add ip address to interface eth0  : 
+ip addr  10.1.1.1/24 dev eth0
+
+#create vxlan interfance and set remote et local going request : 
+ip link add name vxlan10 type id 10 dev eth0 remote 10.1.1.2 local 10.1.1.1 dstport 4789 
+
+#add ip address to vxlan interface :
+ip addr add 20.1.1.1/24 dev vxlan10
+
+#add eth1, vxlan interface into br0 : 
+brctl addif br0 eth1
+brctl addif br0 vxlan10
+
+#bring up vxlan interface :
+ip link set dev vxlan10 up
+
+
