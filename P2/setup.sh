3495764161e0a6c1690b556d3baba78175f2e3a2
Maxime KAYUMBA ONAKASONGO <mkayumba@e1r2p21.clusters.42paris.fr>
2022-08-24 20:15:32 +0200

    update setup.sh

--- P2/setup.sh
+++ P2/setup.sh
@@ -55,3 +55,52 @@ ip link set dev vxlan10 up
 ip addr add 30.1.1.1/24 dev eth1
 #right
 ip addr add 30.1.1.2/24 dev eth1
+
+
+##################multicast LEFT###################
+
+ip link add br0 type bridge
+
+#bring up interface br0 :
+ip link set dev br0 up
+
+#add ip address to interface eth0  :
+
+ip addr add 10.1.1.1/24 dev eth0
+
+
+#left create vxlan interfance and set remote et local going request : 
+ip link add name vxlan10 type vxlan id 10 dev eth0 group 239.1.1.1 dstport 4789
+
+#add ip address to vxlan interface :
+ip addr add 20.1.1.1/24 dev vxlan10
+
+#add eth1, vxlan interface into br0 : 
+brctl addif br0 eth1
+brctl addif br0 vxlan10
+
+#bring up vxlan interface :
+ip link set dev vxlan10 up
+
+
+##################multicast RIGHT###################
+#create a interface br0 :
+ip link add br0 type bridge
+
+#bring up interface br0 :
+ip link set dev br0 up
+
+#right
+ip addr add 10.1.1.2/24 dev eth0
+
+
+ip link add name vxlan10 type vxlan id 10 dev eth0 group 239.1.1.1 dstport 4789
+#add ip address to vxlan interface :
+ip addr add 20.1.1.2/24 dev vxlan10
+
+#add eth1, vxlan interface into br0 : 
+brctl addif br0 eth1
+brctl addif br0 vxlan10
+
+#bring up vxlan interface :
+ip link set dev vxlan10 up
